d7c9b0a1246c745cd12b3cc70c1f6205308b7911
Bernard Lambeau <blambeau@gmail.com>
2010-03-05 11:16:29 +0100

    Create command takes a handler uri as an option.

--- lib/rubyrel/commands/create.rb
+++ lib/rubyrel/commands/create.rb
@@ -1,9 +1,15 @@
 module Rubyrel
   module Commands
     class Create < Rubyrel::Commands::Command
-
+      
+      # Physical handler's uri
+      attr_accessor :handler_uri
+      
       # Contribute to options
       def add_options(opt)
+        opt.on("--handler=URI", "-h", "URI of the physical handler to use") do |value|
+          self.handler_uri = value
+        end
       end
       
       # Returns the command banner
@@ -19,7 +25,9 @@ module Rubyrel
       
       # Creates a Sequel database instance for a given schema
       def create_database(schema)
-        ::Sequel.connect("sqlite://#{schema.name}.db")
+        self.handler_uri = "sqlite://#{schema.name}.db" unless handler_uri
+        info("Using #{handler_uri} as physical handler") if verbose
+        ::Sequel.connect(handler_uri)
       end
       
       # Runs the sub-class defined command
@@ -29,7 +37,7 @@ module Rubyrel
           exit("Unable to find #{schema_file}")  
         end
         schema = Rubyrel::parse_ddl_file(schema_file)
-        schema.install_on!(create_database(schema))
+        schema.install_on!(create_database(schema), {:verbose => verbose})
       end
 
     end # class Check
--- lib/rubyrel/ddl/to_sql/schema.rb
+++ lib/rubyrel/ddl/to_sql/schema.rb
@@ -2,39 +2,48 @@ module Rubyrel
   module DDL
     class Schema
       
+      # Executes some DDL statements
+      def execute_ddl(db, ddl, options = {})
+        return if ddl.empty?
+        puts("Executing: " + ddl) if options[:verbose]
+        db.execute_ddl(ddl)
+      end
+      
       # Installs this schema on a given sequel database
-      def install_on(db)
+      def install_on(db, options = {})
         buffer = ""
         all_objects_in_order.each{|o| 
           sql = o.to_create_sql(db)
           (buffer << sql << ";\n") unless sql.nil? or sql.empty?
         }
-        #puts buffer
-        db.execute_ddl(buffer)
+        execute_ddl(db, buffer, options)
       end
       
       # Forces a re-installation a given database
-      def install_on!(db)
-        uninstall_on!(db)
-        install_on(db)
+      def install_on!(db, options = {})
+        uninstall_on!(db, options)
+        install_on(db, options)
       end
       
       # Installs this schema on a given sequel database
-      def uninstall_on(db)
-        buffer = ""
+      def uninstall_on(db, options = {})
+        buffer, sql = "", ""
         all_objects_in_order.reverse.each{|o| 
           sql = o.to_clean_sql(db)
           (buffer << sql << ";\n") unless sql.nil? or sql.empty?
         }
-        db.execute_ddl(buffer)
+        execute_ddl(db, buffer, options)
       end
       
       # Forces an installation
-      def uninstall_on!(db)
+      def uninstall_on!(db, options = {})
+        sql = ""
         all_objects_in_order.reverse.each{|o| 
-          begin 
-            db.execute_ddl(o.to_clean_sql(db))
-          rescue Sequel::Error
+          begin
+            sql = o.to_clean_sql(db)
+            execute_ddl(db, sql, options)
+          rescue Sequel::Error => ex
+            puts "Ignoring: #{ex.message}" if options[:verbose]
           end
         }
       end
